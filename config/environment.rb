f75fc96d8584609ff774e726d8289d70e958cb58
Chris Mytton <chrismytton@gmail.com>
2010-05-20 23:05:14 +0100

    Pruning the bits that don't need to be tracked.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
-ENV['GEM_PATH'] = File.expand_path('~/.gems') + ':/usr/lib/ruby/gems/1.8'
+
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,11 +15,11 @@ Rails::Initializer.run do |config|
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-   config.gem "multipart"
-  
+  config.gem "multipart"
+
   config.gem "oauth"
-   config.gem "jwagener-oauth-active-resource", :lib => "oauth_active_resource", :source => "http://gems.github.com"
-   config.gem "soundcloud-ruby-api-wrapper", :lib => "soundcloud", :source => "http://gems.github.com"
+  config.gem "jwagener-oauth-active-resource", :lib => "oauth_active_resource", :source => "http://gems.github.com"
+  config.gem "soundcloud-ruby-api-wrapper", :lib => "soundcloud", :source => "http://gems.github.com"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
