d0166de3eafbc3a34698e0a8ecfaa4ae4be1d414
Chris Mytton <chrism@simpleweb.co.uk>
2010-10-01 10:43:25 +0100

    Update the app to the latest version of Rails

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_Cloud-Down_session',
-  :secret      => '874ff1cb0c92ffdf3b4bbfe59d7aa8a403faab5b1f6aac1309780edc0dacbb7702e760316cc0a04dd1e5cf53d850afe594a2d094bba0b421fd2112b8ef92f89d'
-}
+Example::Application.config.session_store :cookie_store, :key => '_CloudDown_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# Example::Application.config.session_store :active_record_store
