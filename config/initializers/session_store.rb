5ed8fb8e3017711ad557bd1ae4b7945206754182
Chris Mytton <chrism@simpleweb.co.uk>
2010-10-01 10:44:42 +0100

    Use CloudDown application

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Example::Application.config.session_store :cookie_store, :key => '_CloudDown_session'
+CloudDown::Application.config.session_store :cookie_store, :key => '_CloudDown_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
